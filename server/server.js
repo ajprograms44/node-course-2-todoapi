d9a83d5e56dde0f901bf32b95b3c8fbb57393e53
Wellington Johnson <wellingtonjohnson@Wellingtons-MacBook-Pro.local>
2018-08-05 13:02:18 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -25,6 +25,18 @@ app.post('/todos', (req,res) => {
 });
 //This is a post request routed to the 'todos' page
 
+app.get('/todos', (req, res) => {
+//get request that returns our request and response objects
+    Todo.find().then((todos) => {
+    //use .find() to fetch all of the todos in the collection and then do something with them
+    //the .then() call to take two functions: the success case function and the reject case function
+        res.send({todos})
+        //send todos in an object back so we can atatch properties later
+    }, (e) => {
+        res.status(400).send(e);
+    })
+})
+
 app.listen(3000, () => {console.log('Started on port 3000')});
 //Starting up app and setting it to listen on port 3000
 
