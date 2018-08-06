a7128cc93464861ff331909e9e62e45fd0a518d3
Wellington Johnson <wellingtonjohnson@Wellingtons-MacBook-Pro.local>
2018-08-05 20:31:51 -0400

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb')
+const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 //Libraries
 
 var {mongoose} = require('./db/mongoose.js')
@@ -82,6 +83,40 @@ app.delete('/todos/:id', (req, res) => {
     })
     
 })
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed'])
+    //We use the '_.pick' method to pull out just the fields we want users to be able to edit
+    //First argument is what we want to edit, in this case the request body
+    //Second argument is an array of what we want users to be able to edit
+    
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+        //We use return so the function can finish here
+    }
+
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo: todo})
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
+
+
+
+
 
 app.listen(port, () => {console.log(`Started on port ${port}`)});
 //Starting up app and setting it to listen on port 3000
